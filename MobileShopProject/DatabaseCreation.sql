b6714071cbd7afdb1d3de400183006ae20780264
idonthaveamiddlename <41285701+idonthaveamiddlename@users.noreply.github.com>
2019-01-15 18:31:55 +0530

    Add files via upload

---
+++ MobileShopProject/DatabaseCreation.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE devank;
+USE devank;
+
+CREATE TABLE CUSTOMER(
+	customer_id INTEGER PRIMARY KEY,
+	cust_name VARCHAR(40),
+	email_id varchar(20),
+	mob_number varchar(15)
+);
+
+CREATE TABLE MOBILE_PHONE(
+	serial_number INTEGER PRIMARY KEY,
+	brand varchar(20),
+	model varchar(20),
+	stock_in_hand INTEGER,
+	cost_in_rupees INTEGER
+);
+
+CREATE TABLE PURCHASE(
+	purchase_id INTEGER PRIMARY KEY,
+	cust_id INTEGER,
+	model_id INTEGER,
+	date_of_purchase DATE,
+	FOREIGN KEY (cust_id) REFERENCES CUSTOMER(serial_number),
+	FOREIGN KEY (model_id) REFERENCES MOBILE_PHONE(serial_number)
+);
+
+INSERT INTO MOBILE_PHONE VALUES(101,'Apple','iPhone X',5,64000);
+INSERT INTO MOBILE_PHONE VALUES(102,'Samsung','Galaxy S9',3,84000);
+INSERT INTO MOBILE_PHONE VALUES(201,'HTC','U11+',6,32000);
+INSERT INTO MOBILE_PHONE VALUES(301,'Google','Pixel 3',1,79000);
+INSERT INTO MOBILE_PHONE VALUES(302,'Mi','Mix 2',8,28500);
